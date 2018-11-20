e1475b7752d96d5d27c1355460dedcaa23fdcf13
AZn5ReD <AZn5ReD>
2018-11-20 21:41:30 +0100

    Ex27

--- ex27/srcs/main.c
+++ ex27/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jchirk <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 12:34:53 by jchirk            #+#    #+#             */
-/*   Updated: 2018/11/20 14:42:36 by jchirk           ###   ########.fr       */
+/*   Updated: 2018/11/20 21:38:49 by jchirk           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 int	main(int argc, char **argv)
 {
 	if (argc < 2)
-		ft_putstr("File name missing.");
+		ft_putstr("File name missing.\n");
 	else if (argc > 2)
-		ft_putstr("Too many arguments.");
+		ft_putstr("Too many arguments.\n");
 	if (argc != 2)
 		return (-1);
 	ft_display_file(argv[1]);
